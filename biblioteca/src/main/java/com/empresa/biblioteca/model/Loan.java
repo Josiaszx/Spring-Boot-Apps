049eaff85d576df04d9189eef06773d3350fcdfc
Josias <maidanajosias220@gmail.com>
2025-12-04 10:17:40 -0300

    completado memeber y loan + realacion entre ellos

--- biblioteca/src/main/java/com/empresa/biblioteca/model/Loan.java
+++ biblioteca/src/main/java/com/empresa/biblioteca/model/Loan.java
@@ -1,5 +1,104 @@
 package com.empresa.biblioteca.model;
 
+import jakarta.persistence.*;
 
+import java.util.Date;
+
+@Entity
+@Table(name = "loans")
 public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private Date loanDate;
+
+    @Column(nullable = false)
+    private Date dueDate;
+
+    @Column(nullable = false)
+    private Date returnDate;
+
+    @Column(nullable = false, length = 20)
+    private String status;
+
+    // constructores y metodos de acceso
+
+    public Loan(Long id, Book book, Member member, Date loanDate, Date dueDate, Date returnDate, String status) {
+        this.id = id;
+        this.book = book;
+        this.member = member;
+        this.loanDate = loanDate;
+        this.dueDate = dueDate;
+        this.returnDate = returnDate;
+        this.status = status;
+    }
+
+    public Loan() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Date getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(Date loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- biblioteca/src/main/java/com/empresa/biblioteca/model/Member.java
+++ biblioteca/src/main/java/com/empresa/biblioteca/model/Member.java
@@ -1,4 +1,112 @@
 package com.empresa.biblioteca.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "members")
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 40)
+    private String memberShipNumber;
+
+    @Column(length = 30, nullable = false)
+    private String firstName;
+
+    @Column(length = 30, nullable = false)
+    private String lastName;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 30, nullable = false, unique = true)
+    private String phone;
+
+    @Column(nullable = false)
+    private Date registrationDate;
+
+    @Column
+    private boolean active;
+
+    public Member(Long id, String memberShipNumber, String firstName, String lastName, String email, String phone, Date registrationDate, boolean active) {
+        this.id = id;
+        this.memberShipNumber = memberShipNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.registrationDate = registrationDate;
+        this.active = active;
+    }
+
+    public Member() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMemberShipNumber() {
+        return memberShipNumber;
+    }
+
+    public void setMemberShipNumber(String memberShipNumber) {
+        this.memberShipNumber = memberShipNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
