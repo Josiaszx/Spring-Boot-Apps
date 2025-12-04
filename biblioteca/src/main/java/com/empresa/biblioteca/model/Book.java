b32fc0550d4bb126f6515928f0f004975778f363
Josias <maidanajosias220@gmail.com>
2025-12-04 10:01:58 -0300

    book y category completado

--- biblioteca/src/main/java/com/empresa/biblioteca/model/Book.java
+++ biblioteca/src/main/java/com/empresa/biblioteca/model/Book.java
@@ -34,5 +34,95 @@ public class Book {
     @JoinColumn(name = "author_id")
     Author author;
 
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    Category category;
 
+
+    // constructores y getters/setters
+    public Book(Long id, String isbn, String title, String publisher, Date publishedDate, Integer availableCopies, Integer totalCopies, Author author, Category category) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.publisher = publisher;
+        this.publishedDate = publishedDate;
+        this.availableCopies = availableCopies;
+        this.totalCopies = totalCopies;
+        this.author = author;
+        this.category = category;
+    }
+
+    public Book() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public Integer getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(Integer availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    public Integer getTotalCopies() {
+        return totalCopies;
+    }
+
+    public void setTotalCopies(Integer totalCopies) {
+        this.totalCopies = totalCopies;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
--- biblioteca/src/main/java/com/empresa/biblioteca/model/Category.java
+++ biblioteca/src/main/java/com/empresa/biblioteca/model/Category.java
@@ -1,4 +1,50 @@
 package com.empresa.biblioteca.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    public Category(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Category() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
