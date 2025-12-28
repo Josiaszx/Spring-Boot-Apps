c550a2aec27dc280d2f4b8b79345d7989b3d72e1
Josias <maidanajosias220@gmail.com>
2025-12-28 13:38:54 -0300

    implementacion completa de api para universidad

--- universidad/src/main/java/com/api/universidad/repository/EstudianteRepository.java
+++ universidad/src/main/java/com/api/universidad/repository/EstudianteRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 
-public interface EstudianteRespository extends JpaRepository<Estudiante, Long> {
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
 
     Optional<Estudiante> findByCodigo(String codigo);
 }
